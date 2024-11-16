375d4f7d11d8af35c478b3f8b9280fe0ac35fd34
501A <art71903design@gmail.com>
2024-11-16 13:44:52 -0800

    Add nav bar to layout component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import AgentInput from "@/components/ui/agent-input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <div className="grid gap-2 bg-zinc-50">
-        <div className="flex items-center justify-center px-6 py-3 gap-6">
-          <Link href="/settings">About</Link>
-          <Link href="/settings">About</Link>
-          <Link href="/settings">Settings</Link>
-        </div>
-        <section className="mx-auto max-w-[1000px] px-6 pt-16 pb-20  w-full flex flex-col justify-center items-center gap-6">
+        <section className="mx-auto max-w-[1000px] px-6 pt-24 pb-28 w-full flex flex-col justify-center items-center gap-6">
           <h1>CompanyName</h1>
           <AgentInput />
         </section>
@@ -20,7 +14,7 @@ export default function Home() {
       <hr />
       <section className="mx-auto max-w-[1000px] w-full grid gap-2 p-6">
         <h2>Workbench</h2>
-        <section className="grid grid-cols-3 gap-2">
+        <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
           <Card>
             <CardHeader>
               <CardTitle>Test</CardTitle>
