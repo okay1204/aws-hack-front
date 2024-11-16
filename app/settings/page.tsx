a29fd0b882c530545d812cfb314fe83b3bceea24
501A <art71903design@gmail.com>
2024-11-16 13:27:39 -0800

    Added nav bar and updated global styles for links

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,23 +1,20 @@
-"use client"
+"use client";
 // import { Card } from '@/components/ui/card';
-import { Label } from '@/components/ui/label';
-import React from 'react'
+import { Label } from "@/components/ui/label";
+import React from "react";
 
 const Settings = () => {
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-    }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
   return (
     <div>
-        <h1 className='font-bold text-2xl'>Settings</h1>
-        <form onSubmit={handleSubmit}>
-            <Label>
-                
-            </Label>
-        </form>
+      <h1 className="font-bold text-2xl">Settings</h1>
+      <form onSubmit={handleSubmit}>
+        <Label></Label>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Settings;
